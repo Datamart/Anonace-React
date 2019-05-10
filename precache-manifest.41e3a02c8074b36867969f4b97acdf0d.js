2e515c7794db4a35d883114f1387c98b1d3789aa
Valentin Podkamennyi <valentin@dtm.io>
2019-05-09 17:20:32 -0700

    Initial version of Anonace React app.

---
+++ precache-manifest.41e3a02c8074b36867969f4b97acdf0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be18fcff96978c0704d13aeedb754f19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0eae8648829af5dd83ef",
+    "url": "/static/css/main.a1ecd78a.chunk.css"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "0eae8648829af5dd83ef",
+    "url": "/static/js/main.2702e188.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
